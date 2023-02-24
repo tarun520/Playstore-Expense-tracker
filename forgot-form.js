b5f8088ff7891b66b809f582dbd06591fb6a095e
tarun520 <78148275+tarun520@users.noreply.github.com>
2023-02-24 10:02:06 +0530

    forgot-password

--- forgot-form.js
+++ forgot-form.js
@@ -1,5 +1,20 @@
-async function forgotpassword(e)
-{   e.preventDefault()
-   
-    await axios.get('http://localhost:3000//password/forgotpassword')
+function forgotpassword(e) {
+    e.preventDefault();
+    console.log(e.target.name);
+    const form = new FormData(e.target);
+
+    const userDetails = {
+        email: form.get("email"),
+
+    }
+    console.log(userDetails)
+    axios.post('http://localhost:3000/password/forgotpassword',userDetails).then(response => {
+        if(response.status === 202){
+            document.body.innerHTML += '<div style="color:red;">Mail Successfuly sent <div>'
+        } else {
+            throw new Error('Something went wrong!!!')
+        }
+    }).catch(err => {
+        document.body.innerHTML += `<div style="color:red;">${err} <div>`;
+    })
 }
